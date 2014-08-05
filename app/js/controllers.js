61cb9b8a9a9cd45968e3ce818475ffa6921b042c
alfred sang <i5ting@126.com>
2014-08-05 10:47:23 +0800

    modify for dynamic_attr

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,22 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
+		$scope.classes = [
+			{
+				"Name": "柯织",
+				"Options" : ["A" , "B", "C"]
+			},
+			{
+				"Name": "雷朋",
+				"Options" : ["A" , "B" , "C" ]
+			}
+		];
 
+		$scope.SelectdCollection = {};
+
+		$scope.submit = function() {
+	     console .log($scope. SelectdCollection);
+		};
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
